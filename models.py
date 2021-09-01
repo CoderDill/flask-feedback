6246c7c1379f823909832de14f9767cb0330fddc
Matthew Dillon <mddillon47@gmail.com>
2021-09-01 13:37:14 -0700

    base template

--- models.py
+++ models.py
@@ -11,3 +11,26 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """User"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    username = db.Column(db.Text,
+                         nullable=False,
+                         unique=True)
+
+    password = db.Column(db.Text,
+                         nullable=False)
+
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+    first_name = db.Column(db.String(30), nullable=False)
+
+    last_name = db.Column(db.String(30), nullable=False)
